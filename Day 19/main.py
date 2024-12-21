50de646360fb6f97dde664a771cea796d5c433a1
Stefan de Laat <stefan1752002@gmail.com>
2024-12-21 23:49:25 +0100

    Finished day 19 part 2

--- Day 19/main.py
+++ Day 19/main.py
@@ -1,4 +1,6 @@
-with open("Day 19/test.txt") as f:
+from functools import cache
+
+with open("Day 19/input.txt") as f:
     lines = [line[:-1] for line in f.readlines()]
     towels = lines[:lines.index("")][0].split(", ")
     designs = lines[lines.index("")+1:]
@@ -11,39 +13,41 @@ for towel in towels:
     else:
         lookup_towels[towel[0]] = [towel]
 
+@cache
 def solve_string(to_solve):
-    print(f"Started with {to_solve}")
-    if to_solve[0] in lookup_towels:
-        if to_solve in lookup_towels[to_solve[0]]: # If whole last part is an towel, directly return true
-            print(f"Found last part: {to_solve}")
-            return to_solve # Return last part
-        else:
-            for lookup_towel in lookup_towels[to_solve[0]]:
-                if not to_solve.startswith(lookup_towel):
-                    continue
-
-                print(f"Used {lookup_towel}")
-                if solve_string(to_solve[len(lookup_towel):]):
-                    print(f"Found inner part: {lookup_towel}")
-                    return lookup_towel # Return inner part
-    return False
-
-def part_one():
     valid = 0
-    for design in designs:
-        if solve_string(design):
-            valid += 1
+    if to_solve[0] in lookup_towels:        
+        for lookup_towel in lookup_towels[to_solve[0]]:
+            if to_solve == lookup_towel:
+                valid += 1
+                continue
+
+            if not to_solve.startswith(lookup_towel):
+                continue
+
+            v = solve_string(to_solve[len(lookup_towel):])
+            if v:
+                valid += v
+
     return valid
 
+def main():
+    valid_unique = 0
+    valid_total = 0
+    for i, design in enumerate(designs):
+        print(f"{i}/{len(designs)}")
+        v = solve_string(design)
+        if v:
+            valid_unique += 1
+            valid_total += v
 
-def part_two():
-    pass
+    print(f"Part one: {valid_unique}")
+    print(f"Part two: {valid_total}")
 
 import time
 startTime = time.time()
 
-print(f"Part one: {part_one()}")
-print(f"Part two: {part_two()}")
+main()
 
 executionTime = (time.time() - startTime)
 print('Execution time in seconds: ' + str(executionTime))
